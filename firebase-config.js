44c54c81abc91f525c40b560716b6229accd1dd4
harpy7777 <harpy7777@gmail.com>
2026-04-30 21:06:59 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5MQx_rXO_xRYelyKxK090TaKa3Gg2D3k",
@@ -8,10 +8,8 @@ const firebaseConfig = {
   projectId: "beta-science",
   storageBucket: "beta-science.firebasestorage.app",
   messagingSenderId: "650652155367",
-  appId: "1:650652155367:web:41d2958e675cb4edd4d142",
-  measurementId: "G-SPNVBHPWP6"
+  appId: "1:650652155367:web:41d2958e675cb4edd4d142"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
